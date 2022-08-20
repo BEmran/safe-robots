24707e1503b4b3b63ae2694829a71df66224ea5b
bara emran <bara.emran@gmail.com>
2022-08-20 00:41:38 -0700

    changes to other mpu app files

--- hardware/navio/app/my_mpu_app.cpp
+++ hardware/navio/app/my_mpu_app.cpp
@@ -5,11 +5,11 @@
 #include <memory>
 
 constexpr mpu::AccelScale ASCALE = mpu::AccelScale::FS_4G;
-constexpr mpu::GyroScale GSCALE = mpu::GyroScale::FS_250DPS;
-constexpr mpu::MagScale MSCALE = mpu::MagScale::FS_16BITS;
-constexpr mpu::MagMode MMODE = mpu::MagMode::CONTINUES_100HZ;
 constexpr mpu::AccelBandWidthHz ABW = mpu::AccelBandWidthHz::BW_21HZ;
+constexpr mpu::GyroScale GSCALE = mpu::GyroScale::FS_250DPS;
 constexpr mpu::GyroBandWidthHz GBW = mpu::GyroBandWidthHz::BW_250HZ;
+constexpr mpu::MagMode MMODE = mpu::MagMode::CONTINUES_100HZ;
+constexpr mpu::MagScale MSCALE = mpu::MagScale::FS_16BITS;
 constexpr uint8_t SAMPLE_RATE_DIVISOR = 4;
 
 //=============================================================================
@@ -20,12 +20,12 @@ int main(int /*argc*/, char** /*argv[]*/)
     return 1;
   }
   mpu::Config config;
-  config.accel_scale = ASCALE;
-  config.gyro_scale = GSCALE;
-  config.mag_scale = MSCALE;
-  config.mag_mode = MMODE;
   config.accel_bw = ABW;
+  config.accel_scale = ASCALE;
   config.gyro_bw = GBW;
+  config.gyro_scale = GSCALE;
+  config.mag_mode = MMODE;
+  config.mag_scale = MSCALE;
   config.sample_rate_divisor = SAMPLE_RATE_DIVISOR;
   auto spi = std::make_unique<SPI>(navio::MPU_SPI_PATH, true);
   auto sensor = std::make_unique<mpu::Mpu9250>(config, std::move(spi), true);
