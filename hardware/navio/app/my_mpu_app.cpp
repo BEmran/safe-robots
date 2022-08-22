fabe8990aee5c8d2bb08b7a8425d70140e5ba3cc
bara emran <bara.emran@gmail.com>
2022-08-22 01:23:25 -0700

    socket mpu

--- hardware/navio/app/my_mpu_app.cpp
+++ hardware/navio/app/my_mpu_app.cpp
@@ -3,6 +3,8 @@
 #include <unistd.h>
 #include <string>
 #include <memory>
+#include <core/utils/writter_file.hpp>
+#include <core/utils/date_time.hpp>
 
 constexpr mpu::AccelScale ASCALE = mpu::AccelScale::FS_16G;
 constexpr mpu::AccelBandWidthHz ABW = mpu::AccelBandWidthHz::BW_44HZ;
@@ -12,6 +14,12 @@ constexpr mpu::MagMode MMODE = mpu::MagMode::CONTINUES_100HZ;
 constexpr mpu::MagScale MSCALE = mpu::MagScale::FS_16BITS;
 constexpr uint8_t SAMPLE_RATE_DIVISOR = 4;
 
+uint TimeInMilliSec()
+{
+  const auto now = std::chrono::system_clock::now();
+  const auto epoch = now.time_since_epoch();
+  return std::chrono::duration_cast<std::chrono::milliseconds>(epoch).count();
+}
 //=============================================================================
 int main(int /*argc*/, char** /*argv[]*/)
 {
@@ -29,21 +37,33 @@ int main(int /*argc*/, char** /*argv[]*/)
   config.sample_rate_divisor = SAMPLE_RATE_DIVISOR;
   auto spi = std::make_unique<SPI>(navio::MPU_SPI_PATH, false);
   auto sensor = std::make_unique<mpu::Mpu9250>(config, std::move(spi), true);
-    
+
   if (!sensor->Probe())
   {
     return EXIT_FAILURE;
   }
   sensor->Initialize();
-  // sensor->Calibrate();
-  
-  //-------------------------------------------------------------------------
 
+  core::utils::FileWritter file("imu.txt");
+  // sensor->Calibrate();
+
+  //-------------------------------------------------------------------------
+  const auto begin = TimeInMilliSec();
   while (true)
   {
-    sensor->Update();
-    std::cout << sensor->GetData();
-    usleep(500000);
+    // sensor->Update();
+    // std::cout << sensor->GetData();
+    // usleep(500000);
+
+    auto raw = sensor->ReadRawData();
+    std::stringstream ss;
+    ss << TimeInMilliSec() - begin << ", "  //
+       << raw.accel.x() << ", " << raw.accel.y() << ", " << raw.accel.z() << ", "  //
+       << raw.gyro.x() << ", " << raw.gyro.y() << ", " << raw.gyro.z() << ", "  //
+       << raw.mag.x() << ", " << raw.mag.y() << ", " << raw.mag.z() << ", "  //
+       << raw.mag_over_flow;
+    file.dump(ss.str());
+    usleep(10000);
   }
   return EXIT_SUCCESS;
 }
