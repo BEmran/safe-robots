254fff6c7bc151b53dd380618da582171aa1c90c
bara emran <bara.emran@gmail.com>
2022-10-03 22:31:19 -0700

    change naming

---
+++ core/test/dependencies/utest_simple_logger.cpp
@@ -0,0 +1,9 @@
+// Copyright (C) 2022 Bara Emran - All Rights Reserved
+
+#include <gtest/gtest.h>
+
+#include <dependencies/simple_logger.hpp>
+
+const TEST(Logger, InaneTest) {
+  depen::Logger("LOGGER")
+}
