0f1cb3859c4de30529591581c6913c5a4e8d3589
bara emran <bara.emran@gmail.com>
2022-10-03 23:59:32 -0700

    simple logger

---
+++ core/include/dependencies/simple_logger.hpp
@@ -0,0 +1,141 @@
+#ifndef DEPENDENCIES_SIMPLE_LOGGER_HPP_
+#define DEPENDENCIES_SIMPLE_LOGGER_HPP_
+
+#include <chrono>
+#include <ctime>
+#include <functional>
+#include <iostream>
+#include <string>
+#include <string_view>
+#include <type_traits>
+
+namespace depend {
+using GetTimeCB = std::function<std::string()>;
+
+/**
+ * @brief get current time
+ *
+ * @return tm* time in time_info_ struct
+ */
+tm* TimeNow() {
+  auto now = std::chrono::system_clock::now();
+  auto now_time_t = std::chrono::system_clock::to_time_t(now);
+  return std::localtime(&now_time_t);
+}
+
+/**
+ * @brief Format time_info_ struct to string by rapping strftime function
+ *.
+ * @param tm time structure contains a calendar date and time broken down into
+ * its components.
+ * @return std::string the formatted string
+ */
+std::string TimeInfo() {
+  auto tm = TimeNow();
+  constexpr auto kMaxSize = 12;
+  std::string buffer(kMaxSize, ' ', std::allocator<char>());
+  strftime(buffer.data(), buffer.size(), " [%H:%M:%S]", tm);
+  return buffer;
+}
+
+/**
+ * @brief Simple logger to be used when logging data
+ * @details
+ * the logged message will lok like: [header TimeInfo(): Msg]
+ * For default header and TimeInfo, the logged message will lok like: [Logger
+ * [xx:xx:xx]: Msg].  Before appending first logged data, the logger will append
+ * a header and TimeInfo(). when std::endl is sent to logger, logger will
+ * consider the message has ended and will append the header and time
+ * information for the next data. If the previous logged message does not end
+ * with std::endl, the Logger append the output stream with std::endl at
+ * destruction.
+ *
+ */
+class Logger {
+  using endl_type = std::ostream&(std::ostream&);  // This is the key: std::endl
+                                                   // is a template function,
+                                                   // and this is the signature
+                                                   // of that function (For
+                                                   // std::ostream).
+ public:
+  static const std::string_view default_header;
+
+  /**
+   * @brief Default constructor to a new Logger object
+   *
+   */
+  Logger() : Logger(default_header, std::cout, TimeInfo) {
+  }
+
+  /**
+   * @brief Construct a new Logger object with a custom log header and output
+   * stream
+   *
+   * @param header log header
+   * @param os output stream
+   */
+  Logger(std::string_view header, std::ostream& os, GetTimeCB time_cb)
+    : m_header(header), m_os(os), m_time_cb(time_cb) {
+  }
+
+  ~Logger() {
+    // stream endl at the end of logging
+    if (!m_first_msg) {
+      m_os << std::endl;
+    }
+  }
+
+  /**
+   * @brief Stream data for std::endl only:
+   *
+   * @param endl end of line
+   * @return Logger& logger reference
+   */
+  Logger& operator<<(endl_type endl) {
+    m_first_msg = true;
+    m_os << endl;
+    return *this;
+  }
+
+  /**
+   * @brief Stream data
+   *
+   * @tparam T any type
+   * @param data data to be logged
+   * @return Logger& logger reference
+   */
+  template <typename T>
+  Logger& operator<<(const T& data) {
+    if (m_first_msg) {
+      m_os << m_header << m_time_cb() << ": ";
+      m_first_msg = false;
+    }
+    m_os << data;
+    return *this;
+  }
+
+ private:
+  bool m_first_msg{true};
+  const std::string m_header;
+  std::ostream& m_os;
+  GetTimeCB m_time_cb;
+};
+
+const std::string_view Logger::default_header = "Logger";
+
+Logger LogDebug() {
+  return Logger("DEBUG", std::cout, TimeInfo);
+}
+
+Logger LogError() {
+  return Logger("ERROR", std::cerr, TimeInfo);
+}
+
+Logger Log(const std::string_view& header, std::ostream& os,
+           GetTimeCB time_cb) {
+  return Logger(header, os, time_cb);
+}
+
+}  // namespace depend
+
+#endif  // DEPENDENCIES_SIMPLE_LOGGER_HPP_
--- core/test/dependencies/utest_simple_logger.cpp
+++ core/test/dependencies/utest_simple_logger.cpp
@@ -3,7 +3,97 @@
 #include <gtest/gtest.h>
 
 #include <dependencies/simple_logger.hpp>
+#include <sstream>
+#include <string_view>
 
-const TEST(Logger, InaneTest) {
-  depen::Logger("LOGGER")
+constexpr std::string_view HEADER = "HEADER";
+using depend::Log;
+using depend::LogDebug;
+using depend::LogError;
+using depend::Logger;
+using namespace std::literals::string_literals;
+
+TEST(Logger, DefaultTest) {
+  const std::string msg = "msg 1";
+  Logger logger;
+  logger << msg << std::endl;
+  EXPECT_TRUE(true);
+}
+
+TEST(Logger, LogDebugTest) {
+  const std::string msg = "debug msg";
+  LogDebug() << msg;
+  EXPECT_TRUE(true);
+}
+
+TEST(Logger, LogErrorTest) {
+  const std::string msg = "error msg";
+  LogError() << msg;
+  EXPECT_TRUE(true);
+}
+
+TEST(Logger, LogUsingLogFunctionAppendEndLine) {
+  std::stringstream ss;
+  auto cb = [] { return std::string{}; };
+  Log("", ss, cb) << "";
+  const std::string expect = ": "s + "\n";
+  EXPECT_EQ(expect, ss.str());
+}
+
+TEST(Logger, LogUsingObjectDoesNotAppendEndLine) {
+  std::stringstream ss;
+  auto cb = [] { return std::string{}; };
+  Logger logger("", ss, cb);
+  logger << "";
+  const std::string expect = ": "s;
+  EXPECT_EQ(expect, ss.str());
+}
+
+TEST(Logger, DoesNotAppendEndLineWhenDestructedAndNotUsed) {
+  std::stringstream ss;
+  auto cb = [] { return std::string{}; };
+  auto logger = new Logger("", ss, cb);
+  delete logger;
+  EXPECT_TRUE(ss.str().empty());
+}
+
+TEST(Logger, DoesNotAppendEndLineWhenDestructedAndPreviousMessageHasEndLine) {
+  std::stringstream ss;
+  auto cb = [] { return std::string{}; };
+  auto logger = new Logger("", ss, cb);
+  *logger << std::endl;
+  EXPECT_EQ("\n", ss.str());
+  ss.str("");  // clear stringstream
+  delete logger;
+  EXPECT_TRUE(ss.str().empty());
+}
+
+TEST(Logger, AppendEndLineWhenDestructedAndPreviousMessageDoesNotHaveEndLine) {
+  std::stringstream ss;
+  auto cb = [] { return std::string{}; };
+  auto logger = new Logger("", ss, cb);
+  *logger << "";
+  EXPECT_EQ(": ", ss.str());
+  ss.str("");  // clear stringstream
+  delete logger;
+  EXPECT_EQ("\n", ss.str());
+}
+
+TEST(Logger, LogUsingLogFunction) {
+  std::stringstream ss;
+  auto cb = [] { return std::string{}; };
+  const std::string msg = "msg 1";
+  Log(HEADER, ss, cb) << msg;
+  const std::string expect = HEADER.data() + ": "s + msg + "\n";
+  EXPECT_EQ(expect, ss.str());
+}
+
+TEST(Logger, ConstructLoggerObject) {
+  std::stringstream ss;
+  auto cb = [] { return std::string{}; };
+  const std::string msg = "msg 1";
+  Logger logger(HEADER, ss, cb);
+  logger << msg;
+  const std::string expect = HEADER.data() + ": "s + msg;
+  EXPECT_EQ(expect, ss.str());
 }
