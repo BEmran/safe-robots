6789bc8e143b50c22691e779690ee84f4d02b2cb
bara emran <bara.emran@gmail.com>
2022-10-15 12:54:34 -0700

    clean up and write comments

--- core/include/core/utils/subject.hpp
+++ core/include/core/utils/subject.hpp
@@ -13,42 +13,55 @@
 
 namespace core::utils {
 
+/**
+ * @brief observer call back to register in a Subject
+ *
+ * @tparam T subject data type
+ */
 template <typename T>
-
 using ObserverCallback = std::function<void(const T& data)>;
 
+/**
+ * @brief General Subject class holds observers' callback to inform when
+ * internal data changes
+ *
+ * @tparam T internal data type
+ */
 template <typename T>
 class Subject {
  public:
+  /**
+   * @brief Construct a new Subject object
+   *
+   * @param name subject name
+   */
   explicit Subject(const std::string& name)
-    : node_(std::make_unique<Node>(CreateSystemNode(name))) {
+    : node_(std::make_unique<Node>(CreatNodeLoggerUsingSystemLogger(name))) {
   }
 
   void Register(std::shared_ptr<ObserverCallback<T>> cb) {
     auto it = observers_.find(cb);
     // TODO(Bara): not needed for Set container
     if (it != observers_.end()) {
-      node_->GetLogger().Warn("This observer has been registered before.\n");
-      return;
+      node_->GetLogger().Warn("This observer has been registered before");
+    } else {
+      observers_.insert(cb);
+      node_->GetLogger().Debug("Registered new observer");
     }
-
-    observers_.insert(cb);
-    node_->GetLogger().Debug("Registered new observer.\n");
   }
 
   void Unregister(std::shared_ptr<ObserverCallback<T>> cb) {
     auto it = observers_.find(cb);
 
     if (it == observers_.end()) {
-      node_->GetLogger().Warn("This observer was not registered.\n");
-      return;
+      node_->GetLogger().Warn("This observer was not registered");
+    } else {
+      observers_.erase(it);
+      node_->GetLogger().Debug("Unregistered an observer");
     }
-
-    observers_.erase(it);
-    node_->GetLogger().Debug("Unregistered an observer.\n");
   }
 
-  void Inform() const {
+  void Notify() const {
     const auto data = Get();
     std::for_each(observers_.begin(), observers_.end(),
                   [&data](auto obs) { (*obs)(data); });
@@ -56,7 +69,7 @@ class Subject {
 
   void Set(const T& data) {
     cash_data_.Set(data);
-    Inform();
+    Notify();
   }
 
   T Get() const {
