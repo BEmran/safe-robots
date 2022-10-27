19d14b5be6f51ecdf0527e756cd349492029bafd
bara emran <bara.emran@gmail.com>
2022-10-26 22:44:04 -0700

    update clock

--- core/include/core/utils/clock.hpp
+++ core/include/core/utils/clock.hpp
@@ -4,38 +4,63 @@
 #define CORE_UTILS_CLOCK_HPP_
 
 #include <chrono>
+#include <iostream>
 #include <memory>
 
 namespace core::utils {
+
+namespace {
+constexpr long MEGA = 1e6;
+constexpr double MICRO = 1e-6;
+
+constexpr double ToSec(const long micro) {
+  return static_cast<double>(micro) * MICRO;
+}
+
+constexpr long ToMicro(const double sec) {
+  return static_cast<long>(sec * MEGA);
+}
+}  // namespace
 
 class TimeStruct {
  public:
   TimeStruct() : secs{0}, micros{0}, in_sec{0.0} {
   }
 
-  TimeStruct(long time_in_micro)
-    : TimeStruct(static_cast<double>(time_in_micro) * 1e-6) {
+  TimeStruct(long time_in_micro) : TimeStruct(ToSec(time_in_micro)) {
   }
 
   TimeStruct(double time_in_sec) : in_sec(time_in_sec) {
     secs = static_cast<long>(in_sec);
-    micros = static_cast<long>((in_sec - secs) * 1e6);
+    micros = ToMicro(in_sec - static_cast<double>(secs));
   }
 
-  long Secs() {
+  inline long Secs() const {
     return secs;
   }
 
-  long Micros() {
+  inline long Micros() const {
     return micros;
   }
 
-  double InSec() {
+  inline double InSec() const {
     return in_sec;
   }
 
-  std::chrono::duration<double> ToChronoDuration() {
+  std::chrono::duration<double> ToChronoDuration() const {
     return std::chrono::duration<double, std::ratio<1>>{in_sec};
+  }
+
+  inline operator double() const {
+    return in_sec;
+  }
+
+  inline std::string ToString() const {
+    return std::to_string(in_sec);
+  }
+
+  inline std::string Pretty() const {
+    return ToString() + " [sec]";
   }
 
  private:
@@ -43,6 +68,10 @@ class TimeStruct {
   long micros{0};
   double in_sec{0.0};
 };
+
+std::ostream& operator<<(std::ostream& os, const TimeStruct& time) {
+  return os << time.ToString();
+}
 
 class TimeInterface {
  public:
